dc1aa783b5b80c77771d465ab4b2c9a8ec3b4563
Moritz <76874869+moritzwgl@users.noreply.github.com>
2026-06-12 10:50:47 +0200

    Set basePath for Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/wc-2026-predictions",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
